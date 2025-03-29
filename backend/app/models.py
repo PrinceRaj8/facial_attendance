bb8605142fbce8f384647953a0b38b22f3d57b66
PrinceRaj8 <princeraj7g5@gmail.com>
2025-03-29 11:10:45 +0530

    Create models.py

---
+++ backend/app/models.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from .database import Base
+import datetime
+
+class User(Base):
+    __tablename__ = "users"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    is_admin = Column(Boolean, default=False)
+    
+    attendance = relationship("Attendance", back_populates="user")
+
+class Attendance(Base):
+    __tablename__ = "attendance"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    timestamp = Column(DateTime, default=datetime.datetime.utcnow)
+    
+    user = relationship("User", back_populates="attendance")
